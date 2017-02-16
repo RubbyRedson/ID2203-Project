13fe4539834e60f9badb8230c9b7390a3927b8da
Victor Axelsson <victor_axelsson@hotmail.com>
2017-02-16 10:05:19 +0100

    Added port numbers

--- scenario/src/main/java/Main.java
+++ scenario/src/main/java/Main.java
@@ -8,7 +8,10 @@ import se.sics.kompics.config.ConfigUpdate;
 import se.sics.kompics.config.Conversions;
 import se.sics.kompics.config.ValueMerger;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.UnknownHostException;
 import java.util.UUID;
 
@@ -24,10 +27,31 @@ public class Main {
         startSlaves(5, "127.0.0.1:45678", -1);
     }
 
+    private static int[] getFreePorts(int count) {
+        int[] ports = new int[count];
+        for(int i = 0; i < count; i++){
+            ports[i] = getFreePort();
+        }
+
+        return ports;
+    }
+
+    private static int getFreePort(){
+        try {
+            ServerSocket s = new ServerSocket(0);
+            return s.getLocalPort();
+        } catch (IOException ex) {
+            return -1;
+        }
+    }
+
     private static void startSlaves(int count, String ipAndPort, int delay){
+
+        int[] ports = getFreePorts(count);
+
         for (int i = 0; i < count; i++){
 
-            String[] slave = {"-c", ipAndPort};
+            String[] slave = {"-c", ipAndPort, "-p", ports[i] + ""};
             se.kth.id2203.Main.main(slave);
 
             if(delay > 0){
