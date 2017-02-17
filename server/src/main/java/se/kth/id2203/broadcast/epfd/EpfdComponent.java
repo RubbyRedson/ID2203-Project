a07d1a5737c5c7bf3607fec311c1cce2a654ad66
RubbyRedson <gurnik2009@gmail.com>
2017-02-17 13:47:22 +0100

    EPFD topology updates from Master

--- server/src/main/java/se/kth/id2203/broadcast/epfd/EpfdComponent.java
+++ server/src/main/java/se/kth/id2203/broadcast/epfd/EpfdComponent.java
@@ -2,11 +2,14 @@ package se.kth.id2203.broadcast.epfd;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.kth.id2203.broadcast.beb.TopologyResponse;
 import se.kth.id2203.broadcast.perfect_link.PL_Deliver;
 import se.kth.id2203.broadcast.perfect_link.PL_Send;
 import se.kth.id2203.broadcast.perfect_link.PerfectLink;
+import se.kth.id2203.networking.Message;
 import se.kth.id2203.networking.NetAddress;
 import se.sics.kompics.*;
+import se.sics.kompics.network.Network;
 import se.sics.kompics.timer.ScheduleTimeout;
 import se.sics.kompics.timer.Timer;
 
@@ -21,16 +24,21 @@ public class EpfdComponent extends ComponentDefinition {
     //******* Ports ******
     protected final Positive<Timer> timer = requires(Timer.class);
     protected final Positive<PerfectLink> pLink = requires(PerfectLink.class);
+    protected final Positive<Network> net = requires(Network.class);
     protected final Negative<EventuallyPerfectFailureDetector> epfd = provides(EventuallyPerfectFailureDetector.class);
     //******* Fields ******
     private NetAddress self = config().getValue("id2203.project.address", NetAddress.class);
+    private NetAddress server = config().getValue("id2203.project.bootstrap-address", NetAddress.class);
     private static int DELAY = 200;
     private int delay = 200;
     private Set<NetAddress> topology = new HashSet<>();
 
-    private Set<NetAddress> alive = new HashSet<>(); //TODO init with topology
+    private Set<NetAddress> alive = new HashSet<>();
     private Set<NetAddress> suspected = new HashSet<>();
     private int seqnum = 0;
+
+    private boolean updateTopology = false;
+    private Set<NetAddress> newTopology = new HashSet<>();
 
     protected final Handler<Start> startHandler = new Handler<Start>() {
         @Override
@@ -38,12 +46,18 @@ public class EpfdComponent extends ComponentDefinition {
             ScheduleTimeout spt = new ScheduleTimeout(delay);
             spt.setTimeoutEvent(new Timeout(spt));
             trigger(spt, timer);
-
         }
     };
     protected final Handler<Timeout> timeoutHandler = new Handler<Timeout>() {
         @Override
         public void handle(Timeout e) {
+            if (updateTopology) {
+                topology = newTopology;
+                alive = copySet(topology);
+                suspected.clear();
+                updateTopology = false;
+            }
+
             if (hasIntersection(alive, suspected)) {
                 delay = delay + DELAY;
             }
@@ -77,6 +91,17 @@ public class EpfdComponent extends ComponentDefinition {
             }
         }
     };
+    protected final ClassMatchedHandler<TopologyResponse, Message> topologyResponseMessageClassMatchedHandler = new ClassMatchedHandler<TopologyResponse, Message>() {
+        @Override
+        public void handle(TopologyResponse topologyResponse, Message message) {
+            newTopology = topologyResponse.topology;
+            updateTopology = true;
+
+            System.out.println("----- Topology received at EPFD ---");
+            System.out.println(newTopology);
+            System.out.println("-----------------");
+        }
+    };
     private boolean hasIntersection(Set a1, Set a2) {
         if (a1.size() == 0 || a2.size() == 0)
             return false;
@@ -95,10 +120,18 @@ public class EpfdComponent extends ComponentDefinition {
         return false;
     }
 
+    private Set<NetAddress> copySet(Set<NetAddress> toCopy) {
+        Set<NetAddress> result = new HashSet<>();
+        for (NetAddress address : toCopy)
+            result.add(address);
+        return result;
+    }
+
     {
         subscribe(startHandler, control);
         subscribe(timeoutHandler, timer);
         subscribe(plDeliverRequestHandler, pLink);
         subscribe(plDeliverResponseHandler, pLink);
+        subscribe(topologyResponseMessageClassMatchedHandler, net);
     }
 }
